029f75d216aa98041132a93da18dfb49b5ef3416
Ted Lawless <lawlesst@gmail.com>
2013-05-03 12:47:23 -0400

    Example with filename.

--- vivo_utils/examples/sparql_select_csv_output.py
+++ vivo_utils/examples/sparql_select_csv_output.py
@@ -12,4 +12,6 @@ WHERE
 }
 """
 
-sparql.results_csv(query)
+#Optionally pass in a filename or path to save the results to CSV.
+#By default results will be saved to the current directory as results.csv
+sparql.results_csv(query, filename="/tmp/myresults.csv")
