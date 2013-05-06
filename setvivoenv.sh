dbe23ae16f948c7b915efacc9867f53134793d45
Ted Lawless <lawlesst@gmail.com>
2013-05-06 16:19:25 -0300

    Update setvivoenv.sh

--- setvivoenv.sh
+++ setvivoenv.sh
@@ -3,4 +3,4 @@
 export VIVO_URL='http://vivo.school.edu/'
 export VIVO_USER='admin@school.edu'
 export VIVO_PASS='pass'
-export VIVO_NAMESPACE='http://school.brown.edu/individual/'
+export VIVO_NAMESPACE='http://vivo.school.edu/individual/'
